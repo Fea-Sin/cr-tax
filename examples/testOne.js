66141d9faf68d5712d6a620ac0bfd7fbda38d0f3
chengqunzhong <feasincc@gmail.com>
2019-09-11 15:11:55 +0800

    样式完成

--- examples/testOne.js
+++ examples/testOne.js
@@ -8,16 +8,48 @@ const reactContainer = document.getElementById('__react-content');
 const bodyContainer = document.getElementsByTagName('body')
 bodyContainer[0].style.padding = '10px'
 reactContainer.style.cssText = `
-                                border: 1px solid #11d0bc;
-                                padding: 10px;
-                                margin-bottom: 10px;
+                                // border: 1px solid #11d0bc;
+                                // padding: 10px;
+                                // margin-bottom: 10px;
                                `
+const boxStyle = {
+  width: 1150,
+  height: 'auto',
+  margin: '0 auto',
+  // border: '1px solid green',
+}
+
+const taxHeader = {
+  number: '2897454',
+  code: '4646456',
+  date: '20190911',
+}
+const taxPurchase = {
+  name: '南京智慧科技有限公司',
+  taxNumber: '142701602922440',
+  addressPhone: 'test 123456',
+  bank: '',
+}
+const taxSales = {
+  totalB: '贰仟零壹拾叁元柒角陆分',
+  totalS: '¥2,013.76',
+  name: '南京云帐房有限公司',
+  taxNumber: '789754654444464',
+  addressPhone: '',
+  bank: '',
+  remark: '',
+}
 
 function render(container) {
   ReactDOM.render(
     <div>
-      <h2>RESCT COMPONENT TEMPLATE</h2>
-      <Testone />
+      <div style={boxStyle}>
+        <Testone
+          taxHeader={taxHeader}
+          taxPurchase={taxPurchase}
+          taxSales={taxSales}
+        />
+      </div>
     </div>, container
   )
 }
